c58a06bafb89197a47b69aaaf11649946c1af0f8
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-06-26 20:33:26 +0300

    high order functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function returnFirstTwoDrivers(drivers) {
+    let newArray = drivers.slice(0,2)
+    return newArray
+}
+
+function returnLastTwoDrivers(drivers) {
+    let newArray = drivers.slice(-2)
+    return newArray
+}
+
+
+function selectDifferentDrivers(drivers , fn) {
+    return fn(drivers)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
